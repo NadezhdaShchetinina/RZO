cc6526dd56bb7097ab3aca1640ff69f8db851a16
NadezhdaShchetinina <118117142+NadezhdaShchetinina@users.noreply.github.com>
2026-04-15 20:37:49 +0300

    Add files via upload

---
+++ lab3/rzo lab3 8sem.cpp
@@ -0,0 +1,34 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat img = imread("markers.png");
+
+    if (img.empty()) {
+        cout << "Ошибка загрузки изображения" << endl;
+        return -1;
+    }
+
+    Mat hsv;
+    cvtColor(img, hsv, COLOR_BGR2HSV);
+
+    Mat mask_rose1, mask_rose2, mask_rose;
+
+    inRange(hsv, Scalar(160, 50, 100), Scalar(180, 255, 255), mask_rose1);
+    inRange(hsv, Scalar(0, 50, 100), Scalar(10, 255, 255), mask_rose2);
+
+    mask_rose = mask_rose1 | mask_rose2;
+
+    imshow("image.png", img);
+    imshow("Rose Mask", mask_rose);
+
+    Mat result;
+    img.copyTo(result, mask_rose);
+    imshow("Rose Objects", result);
+
+    waitKey(0);
+    return 0;
+}
